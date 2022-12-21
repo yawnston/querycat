5ea8f9363d29db32fb6370620ce38ea9debcb62c
Daniel Crha <d.crha@kendaxa.com>
2022-12-21 21:48:19 +0100

    feat(merging): make merging of PostgreSQL + MongoDB work

--- src/merging/instance_merger.py
+++ src/merging/instance_merger.py
@@ -1,5 +1,11 @@
 import json
 from time import sleep
+from querycat.src.open_api_definition_client.api.instance_category_controller.get_instance_object_1 import (
+    sync_detailed as get_instance_object,
+)
+from querycat.src.open_api_definition_client.api.instance_category_controller.get_instance_morphism import (
+    sync_detailed as get_instance_morphism,
+)
 
 from querycat.src.open_api_definition_client.api.job_controller.create_new_job import (
     sync_detailed as create_new_job,
@@ -8,18 +14,29 @@ from querycat.src.open_api_definition_client.api.job_controller.start_job_with_q
     sync_detailed as start_job_with_query,
 )
 from querycat.src.open_api_definition_client.client import Client
+from querycat.src.open_api_definition_client.models.instance_morphism_view import (
+    InstanceMorphismView,
+)
+from querycat.src.open_api_definition_client.models.instance_object_view import (
+    InstanceObjectView,
+)
 from querycat.src.open_api_definition_client.models.job import Job
 from querycat.src.open_api_definition_client.models.new_job_view import NewJobView
 from querycat.src.querying.model import QueryPlan
 
+import httpx
 
 class InstanceMerger:
-    def __init__(self):
+    def __init__(self, schema_id: int):
         self.client = Client(base_url="http://localhost:27500")
+        self.schema_id = schema_id
+        # Instance category manipulation doesn't work without using cookies
+        cookies_response = httpx.get(f"{self.client.base_url}/instances")
+        self.client.cookies = cookies_response.cookies
 
-    def merge(self, query_plan: QueryPlan):
+    def run_job(self, query: str, mapping_id: int, object_key: int, signature: int):
         new_job_view = NewJobView(
-            mapping_id=15,
+            mapping_id=mapping_id,
             type="ModelToCategory",
             name="MyModelToCategory",
         )
@@ -27,13 +44,30 @@ class InstanceMerger:
         job_content_json = json.loads(job_response.content)
         job = Job.from_dict(job_content_json)
 
-        query = """SELECT id, name, surname, address_id FROM customer"""
         start_job_response = start_job_with_query(
             client=self.client, id=job.id, json_body=query
         )
-        sleep(2)
+
+        object_response = get_instance_object(
+            client=self.client, schema_id=self.schema_id, object_key=object_key
+        )
+        object_response_json = json.loads(object_response.content)
+        object = InstanceObjectView.from_dict(object_response_json)
+
+        morphism_response = get_instance_morphism(
+            client=self.client, schema_id=self.schema_id, signature=signature
+        )
+        morphism_response_json = json.loads(morphism_response.content)
+        morphism = InstanceMorphismView.from_dict(morphism_response_json)
+        pass
+
+    def merge(self, query_plan: QueryPlan):
+        pg_query = """SELECT id, name, surname, address_id FROM customer"""
+        mongo_query = """db.order.aggregate( [ { $project : { "_id": 1, "customer_id": 1 } } ] )"""
+        self.run_job(pg_query, 15, 1, 2)
+        self.run_job(mongo_query, 17, 10, 9)
 
 
 if __name__ == "__main__":
-    merger = InstanceMerger()
+    merger = InstanceMerger(schema_id=4)
     merger.merge(None)
